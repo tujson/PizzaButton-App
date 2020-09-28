5b210823f68041c521776f1c5b0df0718f791539
Jason Tu <title.kubo@gmail.com>
2020-09-27 22:12:16 -0400

    Added Twilio call code back

--- common/src/main/java/app/pizzabutton/android/common/PizzaOrderer.kt
+++ common/src/main/java/app/pizzabutton/android/common/PizzaOrderer.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import app.pizzabutton.android.common.models.Store
 import app.pizzabutton.android.common.models.User
 import com.twilio.voice.Call
+import com.twilio.voice.CallException
+import com.twilio.voice.ConnectOptions
+import com.twilio.voice.Voice
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.File
@@ -26,50 +29,50 @@ class PizzaOrderer(private val user: User, private val store: Store) {
         val script = generateScript(user)
 
         generateSpeech(script, context)
-//        val accessToken = getTwilioAccessToken()
-//
-//        val params = hashMapOf<String, String>()
-////        params["to"] = store.phoneNumber THIS WILL CALL THE PIZZA STORE!!!
-//        params["to"] = BuildConfig.TEST_PHONE_NUMBER
-//        // !!!!! DO NOT DELETE UNLESS YOU WANT TO CALL PIZZA STORE
-//        if (params.keys.contains(store.phoneNumber)) {
-//            Log.e(TAG, "Attempting to call pizza store's real phone number.")
-//            return
-//        }
-//        // !!!!! DO NOT DELETE UNLESS YOU WANT TO CALL PIZZA STORE
-//
-//        fileAndMicAudioDevice = FileAndMicAudioDevice(context.applicationContext).also {
-//            Voice.setAudioDevice(it)
-//        }
-//
-//        val connectOptions = ConnectOptions.Builder(accessToken)
-//            .params(params)
-//            .build()
-//        activeCall = Voice.connect(context, connectOptions, object : Call.Listener {
-//            override fun onConnectFailure(call: Call, callException: CallException) {
-//                Log.v(TAG, "Call connect failed")
-//            }
-//
-//            override fun onRinging(call: Call) {
-//                Log.v(TAG, "Call ringing")
-//            }
-//
-//            override fun onConnected(call: Call) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun onReconnecting(call: Call, callException: CallException) {
-//                Log.v(TAG, "Call reconnecting")
-//            }
-//
-//            override fun onReconnected(call: Call) {
-//                Log.v(TAG, "Call reconnected")
-//            }
-//
-//            override fun onDisconnected(call: Call, callException: CallException?) {
-//                TODO("Not yet implemented")
-//            }
-//        })
+        val accessToken = getTwilioAccessToken()
+
+        val params = hashMapOf<String, String>()
+//        params["to"] = store.phoneNumber THIS WILL CALL THE PIZZA STORE!!!
+        params["to"] = BuildConfig.TEST_PHONE_NUMBER
+        // !!!!! DO NOT DELETE UNLESS YOU WANT TO CALL PIZZA STORE. EXTRA CHECK.
+        if (params.keys.contains(store.phoneNumber)) {
+            Log.e(TAG, "Attempting to call pizza store's real phone number.")
+            return
+        }
+        // !!!!! DO NOT DELETE UNLESS YOU WANT TO CALL PIZZA STORE. EXTRA CHECK.
+
+        fileAndMicAudioDevice = FileAndMicAudioDevice(context.applicationContext).also {
+            Voice.setAudioDevice(it)
+        }
+
+        val connectOptions = ConnectOptions.Builder(accessToken)
+            .params(params)
+            .build()
+        activeCall = Voice.connect(context, connectOptions, object : Call.Listener {
+            override fun onConnectFailure(call: Call, callException: CallException) {
+                Log.v(TAG, "Call connect failed")
+            }
+
+            override fun onRinging(call: Call) {
+                Log.v(TAG, "Call ringing")
+            }
+
+            override fun onConnected(call: Call) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onReconnecting(call: Call, callException: CallException) {
+                Log.v(TAG, "Call reconnecting")
+            }
+
+            override fun onReconnected(call: Call) {
+                Log.v(TAG, "Call reconnected")
+            }
+
+            override fun onDisconnected(call: Call, callException: CallException?) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
     private fun getTwilioAccessToken(): String {
